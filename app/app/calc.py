7c9755de47fd46000629ad7fc8ff118c6a85d00a
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-06-12 16:13:57 -0300

    Implement psycopg2 as Postgres Engine

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,4 +4,10 @@ Calculator functions
 
 
 def add(x, y):
+    """Add x and y and return result."""
     return x + y
+
+
+def subtract(x, y):
+    """Subtracts x from y and return result."""
+    return y - x
