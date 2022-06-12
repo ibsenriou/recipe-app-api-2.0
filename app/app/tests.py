203fe13e562b154118af53f3d4810652567145ed
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-06-12 12:25:06 -0300

    Updates

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,10 @@
 from django.test import SimpleTestCase
 
 
-from . import calc
+from .calc import add
 
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(5, 6)
+        res = add(5, 6)
         self.assertEqual(res, 11)
